9cedcafac04214ec0294c64b77d331ea6c28df3d
florindragos <florin@aserto.com>
2023-05-04 13:27:32 +0300

    initial commit

---
+++ magefiles/magefile.go
@@ -0,0 +1,106 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/aserto-dev/mage-loot/buf"
+	"github.com/aserto-dev/mage-loot/common"
+	"github.com/aserto-dev/mage-loot/deps"
+	"github.com/aserto-dev/mage-loot/fsutil"
+)
+
+func init() {
+}
+
+func Deps() {
+	deps.GetAllDeps()
+}
+
+func BuildAll() error {
+	if err := BufBuild(); err != nil {
+		return err
+	}
+	if err := BufGenerate(); err != nil {
+		return err
+	}
+	return Build()
+}
+
+// Build binaries.
+func Build() error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cfg := path.Join(cwd, ".goreleaser.yml")
+	return common.BuildReleaser("--config", cfg, "--clean")
+}
+
+func BufBuild() error {
+	if err := bufModUpdate("proto"); err != nil {
+		return err
+	}
+
+	if err := bufBuild("bin/dsload.bin"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func BufGenerate() error {
+	bufImage := "bin/dsload.bin"
+
+	os.Setenv("BUF_BETA_SUPPRESS_WARNINGS", "1")
+
+	if err := bufGenerate(bufImage); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func bufBuild(bin string) error {
+	fsutil.EnsureDir(path.Dir(bin))
+
+	return buf.Run(
+		buf.AddArg("build"),
+		buf.AddArg("--output"),
+		buf.AddArg(bin),
+	)
+}
+
+func bufModUpdate(dir string) error {
+	return buf.Run(
+		buf.AddArg("mod"),
+		buf.AddArg("update"),
+		buf.AddArg(dir),
+	)
+}
+
+func bufGenerate(image string) error {
+	if err := bufModUpdate("."); err != nil {
+		return err
+	}
+
+	oldPath := os.Getenv("PATH")
+
+	pathSeparator := string(os.PathListSeparator)
+	path := oldPath +
+		pathSeparator +
+		filepath.Dir(deps.GoBinPath("protoc-gen-go-grpc")) +
+		pathSeparator +
+		filepath.Dir(deps.GoBinPath("protoc-gen-go"))
+
+	return buf.RunWithEnv(map[string]string{
+		"PATH": path,
+	},
+		buf.AddArg("generate"),
+		buf.AddArg(image),
+	)
+}
---
+++ pkg/version/version.go
@@ -0,0 +1,67 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// values set by linker using ldflag -X.
+var (
+	ver    string // nolint:gochecknoglobals // set by linker
+	date   string // nolint:gochecknoglobals // set by linker
+	commit string // nolint:gochecknoglobals // set by linker
+)
+
+// Info - version info.
+type Info struct {
+	Version string
+	Date    string
+	Commit  string
+	OS      string
+	Arch    string
+}
+
+// GetInfo gets version stamp information.
+func GetInfo() *Info {
+	if ver == "" {
+		ver = "0.0.0"
+	}
+
+	if date == "" {
+		date = time.Now().Format(time.RFC3339)
+	}
+
+	if commit == "" {
+		commit = "undefined"
+	}
+
+	return &Info{
+		Version: ver,
+		Date:    date,
+		Commit:  commit,
+		OS:      runtime.GOOS,
+		Arch:    runtime.GOARCH,
+	}
+}
+
+// String() returns the version info string.
+func (vi *Info) String() string {
+	return fmt.Sprintf("%s g%s %s-%s [%s]",
+		vi.Version,
+		vi.Commit,
+		runtime.GOOS,
+		runtime.GOARCH,
+		vi.Date,
+	)
+}
+
+func (vi *Info) AsMap() map[string]interface{} {
+	return map[string]interface{}{
+		"version": vi.Version,
+		"date":    vi.Date,
+		"commit":  vi.Commit,
+		"os":      vi.OS,
+		"arch":    vi.Arch,
+	}
+}
