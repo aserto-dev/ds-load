12a88ec6eba13ca6ad9742c7bca080a6d81641f5
carabasdaniel <dani@aserto.com>
2023-05-19 14:30:12 +0300

    Prepare okta fetcher

--- plugins/okta/go.mod
+++ plugins/okta/go.mod
@@ -15,18 +15,29 @@ require (
 	github.com/aserto-dev/ds-load/plugins/okta v0.0.0-00010101000000-000000000000
 	github.com/aserto-dev/ds-load/plugins/sdk v0.0.0-00010101000000-000000000000
 	github.com/aserto-dev/go-grpc v0.8.56
+	github.com/okta/okta-sdk-golang v1.1.0
+	github.com/okta/okta-sdk-golang/v2 v2.18.0
 	github.com/pkg/errors v0.9.1
+	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/aserto-dev/go-directory v0.21.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/patrickmn/go-cache v0.0.0-20180815053127-5633e0862627 // indirect
+	github.com/square/go-jose v2.4.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
-	google.golang.org/grpc v1.54.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- plugins/okta/pkg/app/fetch.go
+++ plugins/okta/pkg/app/fetch.go
@@ -1,7 +1,14 @@
 package app
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
 	"github.com/alecthomas/kong"
+	"github.com/aserto-dev/ds-load/plugins/okta/pkg/oktaclient"
+	"github.com/okta/okta-sdk-golang/v2/okta"
 )
 
 type FetchCmd struct {
@@ -11,6 +18,79 @@ type FetchCmd struct {
 	UserEmail string `cmd:"" env:"DS_OKTA_USER_EMAIL"`
 }
 
-func (cmd *FetchCmd) Run(context *kong.Context) error {
+type OktaPager func(context.Context, *okta.Response, *[]*okta.User) (*okta.Response, error)
+
+func (cmd *FetchCmd) Run(ctx *kong.Context) error {
+
+	oktaClient, err := oktaclient.NewOktaClient(context.Background(), cmd.Domain, cmd.APIToken)
+	if err != nil {
+		return err
+	}
+
+	users, resp, err := oktaClient.ListUsers(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+	err = handleResponse(resp)
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		userBytes, err := json.Marshal(user)
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write(userBytes)
+		os.Stdout.WriteString("\n")
+
+		//Write all user groups
+		groups, resp, err := oktaClient.ListUserGroups(context.Background(), user.Id)
+		if err != nil {
+			return err
+		}
+		err = handleResponse(resp)
+		if err != nil {
+			return err
+		}
+		for _, group := range groups {
+			groupBytes, err := json.Marshal(group)
+			if err != nil {
+				os.Stderr.Write([]byte(err.Error()))
+			}
+			os.Stdout.Write(groupBytes)
+			os.Stdout.WriteString("\n")
+		}
+
+		//Write all user roles
+		roles, resp, err := oktaClient.ListAssignedRolesForUser(context.Background(), user.Id, nil)
+		if err != nil {
+			return err
+		}
+		err = handleResponse(resp)
+		if err != nil {
+			return err
+		}
+		for _, role := range roles {
+			roleBytes, err := json.Marshal(role)
+			if err != nil {
+				os.Stderr.Write([]byte(err.Error()))
+			}
+			os.Stdout.Write(roleBytes)
+			os.Stdout.WriteString("\n")
+		}
+	}
+
+	return nil
+}
+
+func handleResponse(resp *okta.Response) error {
+	if resp.Response != nil && resp.StatusCode == http.StatusTooManyRequests {
+		response, err := json.Marshal(resp)
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write(response)
+	}
 	return nil
 }
