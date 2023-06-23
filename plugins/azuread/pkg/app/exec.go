9108b7e1c52161defef06e5dc6972b0b7d2feabc
florindragos <florin@aserto.com>
2023-06-23 14:51:21 +0300

    add azuread exec

--- plugins/azuread/pkg/app/exec.go
+++ plugins/azuread/pkg/app/exec.go
@@ -1,7 +1,14 @@
 package app
 
 import (
+	"os"
+
 	"github.com/alecthomas/kong"
+	"github.com/aserto-dev/ds-load/sdk/common/js"
+	"github.com/aserto-dev/ds-load/sdk/common/msg"
+	"github.com/aserto-dev/ds-load/sdk/transform"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 type ExecCmd struct {
@@ -10,5 +17,58 @@ type ExecCmd struct {
 }
 
 func (cmd *ExecCmd) Run(context *kong.Context) error {
+	azureClient, err := createAzureAdClient(cmd.Tenant, cmd.ClientID, cmd.ClientSecret, cmd.RefreshToken)
+	if err != nil {
+		return err
+	}
+
+	results := make(chan map[string]interface{}, 1)
+	errCh := make(chan error, 1)
+	go func() {
+		Fetch(azureClient, results, errCh)
+		close(results)
+		close(errCh)
+	}()
+	if err != nil {
+		return err
+	}
+
+	go printErrors(errCh)
+
+	fileReader := os.ReadFile
+	template := cmd.TemplateFile
+	if template == "" {
+		fileReader = Assets().ReadFile
+		template = "assets/transform_template.tmpl"
+	}
+	templateContent, err := fileReader(template)
+	if err != nil {
+		return err
+	}
+	jsonWriter, err := js.NewJSONArrayWriter(os.Stdout)
+	if err != nil {
+		return err
+	}
+	defer jsonWriter.Close()
+	transformer := transform.NewTransformer(cmd.MaxChunkSize)
+	for input := range results {
+		output, err := transformer.TransformToTemplate(input, string(templateContent))
+		if err != nil {
+			return errors.Wrap(err, "transform template execute failed")
+		}
+		var directoryObject msg.Transform
+
+		err = protojson.Unmarshal([]byte(output), &directoryObject)
+		if err != nil {
+			return errors.Wrap(err, "failed to unmarshal transformed data into directory objects and relations")
+		}
+
+		chunks := transformer.PrepareChunks(&directoryObject)
+		err = transformer.WriteChunks(jsonWriter, chunks)
+		if err != nil {
+			return errors.Wrap(err, "failed to write chunks to output")
+		}
+	}
+
 	return nil
 }
