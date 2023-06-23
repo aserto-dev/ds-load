b340d18faa195949222d613dc867714e5e884f5a
Bogdan Irimie <bogdan.irimie@aserto.com>
2023-06-23 09:54:29 +0300

    Simplify load command

--- cli/pkg/app/load.go
+++ cli/pkg/app/load.go
@@ -11,29 +11,18 @@ import (
 
 type LoadCmd struct {
 	clients.Config
-	dirClient clients.DirectoryClient
 }
 
 func (l *LoadCmd) Run(c *cc.CommonCtx) error {
-	err := l.initializeDirectoryClient(c)
-	if err != nil {
-		return err
-	}
-
-	return l.readJSON()
-}
-
-func (l *LoadCmd) initializeDirectoryClient(c *cc.CommonCtx) error {
-	cli, err := clients.NewDirectoryImportClient(c, &l.Config)
+	dirClient, err := clients.NewDirectoryImportClient(c, &l.Config)
 	if err != nil {
 		return errors.Wrap(err, "Could not connect to the directory")
 	}
-	l.dirClient = cli
 
-	return nil
+	return l.processMessagesFromStdIn(dirClient)
 }
 
-func (l *LoadCmd) readJSON() error {
+func (l *LoadCmd) processMessagesFromStdIn(dirClient clients.DirectoryClient) error {
 	reader := bufio.NewReader(os.Stdin)
-	return l.dirClient.HandleMessages(reader)
+	return dirClient.HandleMessages(reader)
 }
