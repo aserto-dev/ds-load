03c84ed43baf88016e1e1b414c417ff63fdcc2bb
Gert Drapers <gert.drapers@live.com>
2024-04-12 14:49:18 -0700

    add auth0 SAML support

--- plugins/auth0/pkg/app/fetch.go
+++ plugins/auth0/pkg/app/fetch.go
@@ -18,6 +18,7 @@ type FetchCmd struct {
 	UserEmail      string `name:"user-email" env:"AUTH0_USER_EMAIL" help:"auth0 user email of the user you want to read" optional:""`
 	Roles          bool   `name:"roles" env:"AUTH0_ROLES" default:"false" negatable:"" help:"include roles"`
 	RateLimit      bool   `name:"rate-limit" default:"true" help:"enable http client rate limiter" negatable:""`
+	SAML           bool   `name:"saml" default:"false" help:"use specialized SAML data loader"`
 }
 
 func (f *FetchCmd) Run(ctx *cc.CommonCtx) error {
@@ -34,7 +35,7 @@ func (f *FetchCmd) Run(ctx *cc.CommonCtx) error {
 	if err != nil {
 		return err
 	}
-	fetcher = fetcher.WithUserPID(f.UserPID).WithEmail(f.UserEmail).WithRoles(f.Roles)
+	fetcher = fetcher.WithUserPID(f.UserPID).WithEmail(f.UserEmail).WithRoles(f.Roles).WithSAML(f.SAML)
 
 	return fetcher.Fetch(ctx.Context, os.Stdout, os.Stderr)
 }
