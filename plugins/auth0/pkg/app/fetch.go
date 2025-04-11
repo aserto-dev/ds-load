a1d0ddc06bf012f14f1e71098c8e1dbc0f587437
florindragos <florin@aserto.com>
2025-04-11 16:11:23 +0300

    pass connection name to auth0 fetcher

--- plugins/auth0/pkg/app/fetch.go
+++ plugins/auth0/pkg/app/fetch.go
@@ -37,7 +37,7 @@ func (f *FetchCmd) Run(ctx *cc.CommonCtx) error {
 		return err
 	}
 
-	fetcher = fetcher.WithUserPID(f.UserPID).WithEmail(f.UserEmail).WithRoles(f.Roles).WithOrgs(f.Orgs).WithSAML(f.SAML)
+	fetcher = fetcher.WithUserPID(f.UserPID).WithEmail(f.UserEmail).WithRoles(f.Roles).WithOrgs(f.Orgs).WithSAML(f.SAML).WithConnectionName(f.ConnectionName)
 
 	return fetcher.Fetch(ctx.Context, os.Stdout, os.Stderr)
 }
